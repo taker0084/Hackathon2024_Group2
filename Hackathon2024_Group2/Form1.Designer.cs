f4cb8970f0166eb9106311f919ae37314c6d3e5e
takeru seki <0084takeru@gmail.com>
2024-08-28 21:44:10 +0900

    変更

--- Hackathon2024_Group2/Form1.Designer.cs
+++ Hackathon2024_Group2/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1519, 740);
             this.Name = "Form1";
             this.ResumeLayout(false);
 
