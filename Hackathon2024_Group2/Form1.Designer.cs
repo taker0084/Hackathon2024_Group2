6b51bff934ee1af5d501159f3eb535de5ef778c1
takeru seki <0084takeru@gmail.com>
2024-08-29 07:23:09 +0900

    サイズ変更

--- Hackathon2024_Group2/Form1.Designer.cs
+++ Hackathon2024_Group2/Form1.Designer.cs
@@ -44,76 +44,70 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(148, 100);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(114, 75);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(267, 62);
+            this.label1.Size = new System.Drawing.Size(236, 46);
             this.label1.TabIndex = 0;
-            this.label1.Text = "今日の日付";
+            this.label1.Text = "今日の日付：";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(148, 204);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(114, 186);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(170, 24);
+            this.label2.Size = new System.Drawing.Size(272, 46);
             this.label2.TabIndex = 1;
             this.label2.Text = "誕生日が近い人";
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("游明朝 Demibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 24;
-            this.listBox1.Location = new System.Drawing.Point(139, 313);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.ItemHeight = 36;
+            this.listBox1.Location = new System.Drawing.Point(107, 235);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(603, 604);
+            this.listBox1.Size = new System.Drawing.Size(465, 436);
             this.listBox1.TabIndex = 2;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(1066, 208);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Location = new System.Drawing.Point(726, 153);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(129, 31);
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(1409, 208);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Location = new System.Drawing.Point(982, 153);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(129, 31);
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
             this.textBox2.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1256, 213);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(883, 160);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 24);
+            this.label3.Size = new System.Drawing.Size(26, 18);
             this.label3.TabIndex = 5;
             this.label3.Text = "月";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(1617, 217);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(1176, 156);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(34, 24);
+            this.label4.Size = new System.Drawing.Size(26, 18);
             this.label4.TabIndex = 6;
             this.label4.Text = "日";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("游明朝 Demibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button1.Location = new System.Drawing.Point(1262, 433);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(971, 325);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(322, 115);
+            this.button1.Size = new System.Drawing.Size(248, 86);
             this.button1.TabIndex = 7;
             this.button1.Text = "検索";
             this.button1.UseVisualStyleBackColor = true;
@@ -122,10 +116,9 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("游明朝 Demibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button2.Location = new System.Drawing.Point(1262, 579);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(971, 434);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(322, 121);
+            this.button2.Size = new System.Drawing.Size(248, 91);
             this.button2.TabIndex = 8;
             this.button2.Text = "RESET";
             this.button2.UseVisualStyleBackColor = true;
@@ -134,17 +127,19 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(422, 126);
+            this.label5.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(340, 75);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 24);
+            this.label5.Size = new System.Drawing.Size(114, 46);
             this.label5.TabIndex = 9;
             this.label5.Text = "label5";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1975, 987);
+            this.ClientSize = new System.Drawing.Size(1519, 740);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -155,7 +150,6 @@
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
